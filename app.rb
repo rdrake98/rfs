205a1b12f2ddfd93a5727840bc75f10a9f3c3e6b
Richard Drake <rdrake98@gmail.com>
2018-07-13 19:48:02 +0100

    dev changes phase 2

--- app.rb
+++ app.rb
@@ -11,11 +11,11 @@ puts $wikis[:dev].edition
 class App < Roda
   route do |r|
     r.on "public" do
-      r.post "save_tiddler" do
+      r.post "change_tiddler" do
         p = r.params
         wiki_name = p['wiki'].to_sym
         title = p['name']
-        message = "#{title} changed in #{wiki_name}"
+        message = "#{title} #{p['action']} in #{wiki_name}"
         puts message
         wiki = $wikis[wiki_name]
         wiki.add_changes(p['changes'])
