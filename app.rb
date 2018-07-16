c97eb8fbcdbc3d6f7bee61c6e183681aa4780e97
Richard Drake <rdrake98@gmail.com>
2018-07-16 18:29:46 +0100

    initial ajax saving

--- app.rb
+++ app.rb
@@ -14,29 +14,26 @@ class App < Roda
     r.on "public" do
       r.post "change_tiddler" do
         p = r.params
-        wiki_name = p['wiki'].to_sym
-        message = "#{p['title']} #{p['action']} in #{wiki_name}"
+        wiki_type = p['wiki'].to_sym
+        message = "#{p['title']} #{p['action']} in #{wiki_type}"
         puts message
-        wiki = $wikis[wiki_name]
-        wiki.add_changes(p['changes'])
+        $wikis[wiki_type]&.add_changes(p['changes'])
         message
+      end
+
+      r.post "save" do
+        p = r.params
+        wiki_type = p['wiki'].to_sym
+        puts "saving #{wiki_type}"
+        $wikis[wiki_type]&.save(p['edition'])
+        puts $wikis[:dev].edition + " from rb"
+        "#{wiki_type} saved"
       end
     end
 
     r.get "local" do
       puts $wikis[:fat]['rfs'].content
       "local only"
-    end
-
-    r.get "try_save" do
-      # byebug
-      $wikis[:dev].try_save
-      "save done"
-    end
-
-    r.get "try_backup" do
-      $wikis[:dev].try_backup
-      "backup done"
     end
   end
 end
