a435d769ab887f513d8839af8fbf60df935f62ab
Richard Drake <rdrake98@gmail.com>
2018-07-18 20:19:46 +0100

    safer if not fat or dev

--- app.rb
+++ app.rb
@@ -2,29 +2,43 @@ require 'roda'
 require 'splitter'
 
 $wikis = {
-  fat: Splitter.fat,
-  dev: Splitter.dev
+  "fat" => Splitter.fat,
+  "dev" => Splitter.dev
 }
-puts $wikis[:fat].edition
-puts $wikis[:dev].edition
+puts $wikis["fat"].edition
+puts $wikis["dev"].edition
 
 class App < Roda
   route do |r|
     r.on "public" do
       r.post "change_tiddler" do
         p = r.params
-        wiki_type = p['wiki'].to_sym
-        message = "#{p['title']} #{p['action']} in #{wiki_type}"
-        puts message
-        $wikis[wiki_type]&.add_changes(p['changes'])
-        message
+        wiki_type = p['wiki']
+        wiki = $wikis[wiki_type]
+        if wiki
+          message = "#{p['title']} #{p['action']} in #{wiki_type}"
+          puts message
+          wiki.add_changes(p['changes'])
+          message
+        else
+          message = "#{p['title']} not #{p['action']} for #{wiki_type}"
+          puts message
+          message
+        end
       end
 
       r.post "save" do
-        wiki_type = r.params['wiki'].to_sym
-        puts "saving #{wiki_type}"
-        $wikis[wiki_type]&.save
-        "#{wiki_type} saved"
+        p = r.params
+        wiki_type = p['wiki']
+        wiki = $wikis[wiki_type]
+        if wiki
+          puts "saving #{wiki_type}"
+          wiki.save
+          wiki.edition
+        else
+          puts "saving not supported for #{wiki_type}"
+          ""
+        end
       end
     end
 
