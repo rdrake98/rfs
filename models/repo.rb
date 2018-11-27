eb45d2b1ce087b641869c7e8925ade0aa4a5724b
Richard Drake <rdrake98@gmail.com>
2018-11-27 19:38:21 +0000

    limited commits

--- models/repo.rb
+++ models/repo.rb
@@ -7,8 +7,9 @@ RepoFile = Struct.new(:name, :size)
 
 class Repo
 
-  def initialize(dir=ENV["compiled"])
+  def initialize(dir=ENV["compiled"], limit=999999)
     @repo = Rugged::Repository.new(dir)
+    @limit = limit
   end
 
   def commits
@@ -20,7 +21,7 @@ class Repo
     walker = Rugged::Walker.new(@repo)
     walker.push(@repo.head.target_id)
     @commits = []
-    walker.each { |c| @commits << c }
+    walker.each { |c| @commits << c; break if @commits.size >= @limit }
     @commits
   end
 
