7e742607e44d1feaea72b7532bb29270f2a44edc
Richard Drake <rdrake98@gmail.com>
2018-12-21 11:57:41 +0000

    show times

--- models/splitr.rb
+++ models/splitr.rb
@@ -46,13 +46,14 @@ class Splitr < Splitter
     filename
   end
 
-  def self.show_history(n=nil, step=100)
+  def self.show_history(n=13, just_one=false, step=100)
+    $last_save = Time.new(0)
     dirs = Dir.glob "/Volumes/SH1/fatword/*"
-    range = n ? (n..n) : (13...dirs.size)
+    range = just_one ? (n..n) : (n...dirs.size)
     range.each {|n| puts "", n; show_sample(dirs[n], step)}
-    unless n
+    unless just_one && n < dirs.size
       puts "", dirs.size
-      show_sample("/Volumes/SH1/_backup", step) unless n
+      show_sample("/Volumes/SH1/_backup", step)
       puts "", range.size + 1
     end
   end
@@ -67,10 +68,25 @@ class Splitr < Splitter
       print f, " "
       begin
         w = new(f)
-        puts w.tiddlers.size
+        save = w.save_time
+        tiddler = w.tidder_time
+        puts "#{w.tiddlers.size} - #{save.to_minute 2} - #{tiddler.to_minute 2}"
+        puts "*** save before tiddler ***" if save < tiddler
+        puts "*** save before last save ***" if save < $last_save
+        $last_save = save
       rescue => e
         puts e
       end
     end
+  end
+
+  def save_time
+    s = @filename.gsub(/\D/, "")
+    s = "20" + s if s.size == 12
+    Time.new(s[0..3],s[4..5],s[6..7],s[8..9],s[10..11],s[12..13])
+  end
+
+  def tidder_time
+    tiddlers.map(&:modified).max
   end
 end
