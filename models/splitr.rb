56989750c82d1b7dc70824b6574fd6614a91de3f
Richard Drake <rdrake98@gmail.com>
2019-01-02 18:50:47 +0000

    copy problem files for mg

--- models/splitr.rb
+++ models/splitr.rb
@@ -91,4 +91,14 @@ class Splitr < Splitter
   def tidder_time
     tiddlers.map(&:modified).max
   end
+
+  def self.problem_files
+    files = Dir.glob "/Volumes/SH1/fatword/*/*.html"
+    File.readlines("/Users/rd/Dropbox/_shared/problems.txt").map{|line|
+      files.find{|full_name| full_name[line.split(" ")[0]]}}
+  end
+
+  def self.cp_problem_files
+    problem_files.each {|f| `cp #{f} /Users/rd/Dropbox/_shared`}
+  end
 end
