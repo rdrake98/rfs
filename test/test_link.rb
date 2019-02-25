1575b5fa23f93731b4a33b5d7355181fec6ab3ee
Richard Drake <rdrake98@gmail.com>
2019-02-25 11:11:37 +0000

    refactor

--- test/test_link.rb
+++ test/test_link.rb
@@ -14,15 +14,13 @@ class TestLink < MiniTest::Test
   if ARGV[0] == "c"; `cp $dev #{wiki_path}`; end
   wiki = Splitter.new(wiki_path)
   %w[TL01 TL02 TL03].each do |name|
-    t = wiki[name]
-    ta = wiki["#{name}A"]
     describe name do
-      tests = ta.content.split("----\n")
+      tests = wiki["#{name}A"].content.split("----\n")
       tests.each do |test|
         search_text, result = test.split("\n")
         # binding.pry if $dd
         it search_text do
-          assert_has_line(t.link(search_text, false, false), result)
+          assert_has_line(wiki[name].link(search_text, false, false), result)
         end
       end
     end
