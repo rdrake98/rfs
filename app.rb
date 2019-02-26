3cfac6520f051cce91c265d517692bd6042306e6
Richard Drake <rdrake98@gmail.com>
2019-02-26 19:56:42 +0000

    WIKIS

--- app.rb
+++ app.rb
@@ -3,20 +3,20 @@ require 'splitter'
 require 'chartkick'
 require 'repo_compiled'
 
-$wikis = {"fat" => Splitter.fat, "dev" => Splitter.dev}
-puts $wikis["fat"].edition
-puts $wikis["dev"].edition
-$repo = RepoCompiled.new
-
 class App < Roda
   plugin :render
   plugin :assets, js: ['Chart.bundle.min.js', 'chartkick.js']
   plugin :h
 
+  WIKIS = {"fat" => Splitter.fat, "dev" => Splitter.dev}
+  puts WIKIS["fat"].edition
+  puts WIKIS["dev"].edition
+  REPO = RepoCompiled.new
+
   def reload(type="fat", saving=true, edition=nil, changes=nil)
     puts "reloading #{type}"
     wiki = type == "fat" ? Splitter.fat : Splitter.dev
-    $wikis[type] = wiki
+    WIKIS[type] = wiki
     saving ? edition ? wiki.save(edition, changes) : wiki.do_save : wiki
   end
 
@@ -26,7 +26,7 @@ class App < Roda
       r.post "change_tiddler" do
         p = r.params
         type = p['type']
-        wiki = $wikis[type]
+        wiki = WIKIS[type]
         message = "#{p['title']} #{p['action']} in #{type}"
         puts message
         wiki.add_changes(p['changes']) if wiki
@@ -38,7 +38,7 @@ class App < Roda
         p = r.params
         type, title, name, edition = p['type'],p['title'],p['name'],p['edition']
         puts "#{p['action']} '#{name}' in #{title} in #{type}"
-        wiki = $wikis[type] || (w = Splitter.new(type); type = nil; w)
+        wiki = WIKIS[type] || (w = Splitter.new(type); type = nil; w)
         clash = wiki.check_file_edition(edition)
         if clash
           response["clash"] = clash.split(",")[0]
@@ -59,18 +59,18 @@ class App < Roda
       r.post "save" do
         p = r.params
         type, edition, changes = p['type'], p['edition'], p['changes']
-        wiki = $wikis[type] || Splitter.new(type)
+        wiki = WIKIS[type] || Splitter.new(type)
         wiki.save(edition, changes) || reload(type, true, edition, changes)
       end
     end
 
     r.get "graph" do
       chartkick = Class.new.include(Chartkick::Helper).new
-      view('graph', locals: {chartkick: chartkick, repo: $repo})
+      view('graph', locals: {chartkick: chartkick, repo: REPO})
     end
 
     r.get "sync" do
-      $wikis["fat"].sync
+      WIKIS["fat"].sync
     end
 
     r.get "force" do
