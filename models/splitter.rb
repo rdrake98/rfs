758936d07e06b01b905b26a5d9ac085e45a9a878
Richard Drake <rdrake98@gmail.com>
2019-02-27 17:08:00 +0000

    sharing changes thru dropbox

--- models/splitter.rb
+++ models/splitter.rb
@@ -245,7 +245,11 @@ class Splitter
   end
 
   def add_changes(json)
-    File.write(changes_file, json + "\n") if @type
+    if @type
+      text = json + "\n"
+      File.write(changes_file, text)
+      File.write("/Users/rd/Dropbox/_changes/m#{@host}_#{@type}.json", text)
+    end
   end
 
   def commit_changes_file(message)
