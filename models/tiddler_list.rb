cc993d06b4a8e35a0dd43e0bf3ac70f964c529dd
Richard Drake <rdrake98@gmail.com>
2019-03-16 07:14:11 +0000

    minimal disruption to TiddlerList to work on mp

--- models/tiddler_list.rb
+++ models/tiddler_list.rb
@@ -22,27 +22,29 @@ class TiddlerList
   end
 
   def self.setup
-    set_git("5Mar19")
+    set_git(git_tag)
     fat = Splitter.new(html_path("_", ""))
     excluded = fat.titles_linked("MacrosNotTo") +
       fat.titles_linked("AcceptableDifferences")
-    self._all = names_from(html_path("_output", "")) - excluded
-    set_git("apr01")
-    self._all1 = names_from(html_path("_output", "")) - excluded
-    self._fail1 = names_from(html_path) - excluded
-    set_git("mar23")
-    self._all2 = names_from(html_path("_output", "")) - excluded
-    self._fail2 = (1..154).map do |n|
-      path_rb = html_path(sprintf('%03i',n))
-      path_js = path_rb.gsub(/rb/, "js")
-      js_hash = {}
-      Regex.scan_output(path_js).each {|name, output| js_hash[name] = output}
-      Regex.scan_output(path_rb).
-        select{|name, output| output != js_hash[name]}.map{|name, output| name}
-    end.flatten - excluded
+    if !mp?
+      self._all = names_from(html_path("_output", "")) - excluded
+      set_git("apr01")
+      self._all1 = names_from(html_path("_output", "")) - excluded
+      self._fail1 = names_from(html_path) - excluded
+      set_git("mar23")
+      self._all2 = names_from(html_path("_output", "")) - excluded
+      self._fail2 = (1..154).map do |n|
+        path_rb = html_path(sprintf('%03i',n))
+        path_js = path_rb.gsub(/rb/, "js")
+        js_hash = {}
+        Regex.scan_output(path_js).each {|name, output| js_hash[name] = output}
+        Regex.scan_output(path_rb).
+          select{|name, output| output != js_hash[name]}.map{|name, output| name}
+      end.flatten - excluded
+    end
     self._excluded = excluded
     self._shadows = fat["ShadowTiddlersFinal"].content.split("\n")
-    set_git("test_only")
+    set_git(git_branch)
     fat
   end
 
