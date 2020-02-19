77bf638c07c833346e43ec7b5cb0ce0cf38faee1
Richard Drake <rdrake98@gmail.com>
2020-02-19 16:19:04 +0000

    step

--- bin/add_links.rb
+++ bin/add_links.rb
@@ -5,14 +5,11 @@ require 'json'
 
 Dir.chdir('../link_data/exports')
 names = Dir.glob("*.txt").reverse
-puts names
-puts names.map {|n| File.read(n)[2..-1].lines[0]}
 contents = names.map {|n| File.read(n)[2..-1]}
-# puts contents[1]
-target = contents.find {|c| c.lines[0].chomp == 'Sabisky'}
+target = contents.find {|c| c.lines[0].chomp == ARGV[0]}
+return unless target
 puts target&.size
-# target = names.find {|n| File.read(n)[2..-1].lines[0] == "Sabisky"}
-# puts target
+
 # puts "#{json.size} bytes of json"
 # backup = JSON.parse(json)
 # sessions = backup["sessions"]
