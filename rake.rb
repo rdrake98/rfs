737829615acabeaba93d35a8425d7d9c023a86ee
Richard Drake <rdrake98@gmail.com>
2020-02-26 16:43:29 +0000

    rake extended

--- rake.rb
+++ rake.rb
@@ -1,6 +1,8 @@
 # rake.rb
 
-if ARGV[0]
+if ARGV[0] == 'e'
+  require './test/test_externals.rb'
+elsif ARGV[0]
   require './test/quick_fat.rb'
 else
   Dir.glob('./test/test*.rb')[2..-1].each { |file| require file }
