38d872e27612cbccb18afa510bd32800f5662edf
Richard Drake <rdrake98@gmail.com>
2020-04-03 00:56:42 +0100

    separate out show_sessions

--- bin/show_sessions.rb
+++ bin/show_sessions.rb
@@ -6,6 +6,8 @@ dir = "/Users/rd/Dropbox/_shared/link_data"
 json = File.read("#{dir}/backups/s200402.232908p.js")[2..-1]
 puts "#{json.size} bytes of json"
 sessions = JSON.parse(json)["sessions"]
-puts "#{sessions.size} sessions"
-windows = sessions[0]["windows"]
+types = sessions.group_by {|s| s["type"]}.map {|t, l| ", #{l.size} #{t}"}.join
+puts "#{sessions.size} sessions#{types}"
+current = sessions[0]
+windows = current["windows"]
 puts "#{windows.size} windows in current session"
