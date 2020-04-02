508b3f006d9b00a26b87a8880610204676e2b2ec
Richard Drake <rdrake98@gmail.com>
2020-04-03 00:52:53 +0100

    show_links more compact output

--- bin/show_links.rb
+++ bin/show_links.rb
@@ -3,12 +3,12 @@
 require 'json'
 
 dir = "/Users/rd/Dropbox/_shared/link_data"
-json = File.read("#{dir}/backups/s200310.030533g.js")[2..-1]
+json = File.read("#{dir}/backups/s200402.232908p.js")[2..-1]
 puts "#{json.size} bytes of json"
 backup = JSON.parse(json)
 sessions = backup["sessions"]
-puts "#{sessions.size} sessions"
-sessions.group_by {|s| s["type"]}.each {|t, list| puts "#{t} #{list.size}"}
+types = sessions.group_by {|s| s["type"]}.map {|t, l| ", #{l.size} #{t}"}.join
+puts "#{sessions.size} sessions#{types}"
 current = sessions[0]
 windows = current["windows"]
 puts "#{windows.size} windows in current session"
