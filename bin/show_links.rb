edfc5bac76fb1c752507260b750087e441b6de5f
Richard Drake <rdrake98@gmail.com>
2020-04-04 02:56:34 +0100

    three significant figures

--- bin/show_links.rb
+++ bin/show_links.rb
@@ -5,7 +5,7 @@ require 'json'
 Dir.chdir("/Users/rd/Dropbox/_shared/link_data/backups")
 names = Dir.glob("s*p.js").sort.reverse
 json = File.read(names[0])[2..-1]
-puts "#{names[0][0..-4]}: #{(json.size/1000000.0).round(2)}MB"
+puts "#{names[0][0..-4]}: #{"%.#{3}g" % (json.size/1000000.0)}MB"
 sessions = JSON.parse(json)["sessions"]
 windows = sessions[0]["windows"]
 puts "#{sessions.size} sessions, #{windows.size} windows in current"
