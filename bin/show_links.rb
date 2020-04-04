011514c5d1658a1be207580fb75173e4e5ecb648
Richard Drake <rdrake98@gmail.com>
2020-04-04 03:18:01 +0100

    summary of all mp backups

--- bin/show_links.rb
+++ bin/show_links.rb
@@ -3,11 +3,12 @@
 require 'json'
 
 Dir.chdir("/Users/rd/Dropbox/_shared/link_data/backups")
-names = Dir.glob("s*p.js").sort.reverse
-json = File.read(names[0])[2..-1]
-puts "#{names[0][0..-4]}: #{"%.#{3}g" % (json.size/1000000.0)}MB"
-sessions = JSON.parse(json)["sessions"]
-windows = sessions[0]["windows"]
-puts "#{sessions.size} sessions, #{windows.size} windows in current"
-tabs = windows[0]["tabs"]
-puts tabs[-1]
+Dir.glob("s*p.js").sort.each do |name|
+  json = File.read(name)[2..-1]
+  sessions = JSON.parse(json)["sessions"]
+  windows = sessions[0]["windows"]
+  tab = windows[0]["tabs"][-1]
+  puts "#{name[0..-4]}: #{'%.3g' % (json.size/1000000.0)}MB, " +
+    "#{sessions.size} sessions, #{windows.size} windows in current"
+  puts [tab['windowId'], tab['index'], tab['id'], tab['title']].join(", "), ""
+end
