0f57b219469103da7e7bd89000edebd11ced4e80
Richard Drake <rdrake98@gmail.com>
2020-04-28 04:32:10 +0100

    updated gca to include bitbucket not gitlab

--- models/wiki_with_tabs_sb.rb
+++ models/wiki_with_tabs_sb.rb
@@ -3,7 +3,7 @@
 require 'wiki_with_tabs'
 
 class WikiWithTabsSB < WikiWithTabs
-  def initialize(sb_file, wiki_file=nil)
+  def initialize(sb_file="s200428.013610p.js", wiki_file=nil)
     @sb_file = sb_file
     super(nil, wiki_file)
   end
@@ -16,7 +16,11 @@ class WikiWithTabsSB < WikiWithTabs
   end
 
   def file_links
-    []
+    Dir.chdir("/Users/rd/Dropbox/_shared/link_data/backups")
+    json = File.read(@sb_file)[2..-1]
+    sessions = JSON.parse(json)["sessions"]
+    windows = sessions[0]["windows"]
+    windows.map {|window| FileLinksSB.new(window)}
     # pattern = "/Users/rd/ww/tabs/#{tabs_dir}/*#{two_level ? "/*" : ""}"
     # Dir[pattern].map{|name| FileLinks.new(name, @two_level)}
   end
