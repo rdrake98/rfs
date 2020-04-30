b644ed4ca5ff26ca788fba1750956dcb42d23ffc
Richard Drake <rdrake98@gmail.com>
2020-04-30 03:34:07 +0100

    use latest SessionBuddy backup if not provided

--- models/wiki_with_tabs_sb.rb
+++ models/wiki_with_tabs_sb.rb
@@ -5,6 +5,7 @@ require 'wiki_with_tabs'
 class WikiWithTabsSB < WikiWithTabs
   def initialize(sb_file, wiki_file=nil)
     Dir.chdir("/Users/rd/Dropbox/_shared/link_data/backups")
+    sb_file ||= Dir.glob("s*p.js").sort[-1]
     json = File.read(sb_file)[2..-1]
     windows = JSON.parse(json)["sessions"][0]["windows"]
     @file_links = windows.map {|window| FileLinksSB.new(window)}
