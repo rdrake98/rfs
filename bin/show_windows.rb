6eb721a7d1f86f060a9530706de1dc5ca1fdd30f
Richard Drake <rdrake98@gmail.com>
2020-05-06 06:59:45 +0100

    sequences on tap

--- bin/show_windows.rb
+++ bin/show_windows.rb
@@ -9,18 +9,20 @@ backups = Dir.glob("s*p.js").sort.map do |name|
   ids = JSON.parse(json)["sessions"][0]["windows"].map{|win| win['id']}
   Backup.new(name[0..-4], ids)
 end
-Sequence = Struct.new(:id, :backup1, :backup2)
-results = []
+Sequence = Struct.new(:id, :length, :backup1, :backup2)
+sequences = []
 previous = nil
 backups.each do |backup|
+  name = backup.name
   backup.windows.each do |id|
-    prev = results.find {|seq| seq.id == id && seq.backup2 == previous}
+    prev = sequences.find {|seq| seq.id == id && seq.backup2 == previous}
     if prev
-      prev.backup2 = backup.name
+      prev.backup2 = name
+      prev.length += 1
     else
-      results << Sequence.new(id, backup.name, backup.name)
+      sequences << Sequence.new(id, 1, name, name)
     end
   end
-  previous = backup.name
+  previous = name
 end
-puts results
+puts sequences
