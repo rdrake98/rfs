47d0b02fbf4f284b7df20d1f1417e78b12952e0f
Richard Drake <rdrake98@gmail.com>
2020-05-06 05:50:31 +0100

    refactor

--- bin/show_windows.rb
+++ bin/show_windows.rb
@@ -4,10 +4,7 @@ require 'json'
 
 Dir.chdir(ENV['tab_backups'])
 Dir.glob("s*p.js").sort.each do |name|
-  json = File.read(name)[2..-1]
-  sessions = JSON.parse(json)["sessions"]
-  windows = sessions[0]["windows"]
-  tab = windows[0]["tabs"][-1]
+  windows = JSON.parse(File.read(name)[2..-1])["sessions"][0]["windows"]
   puts "%s: %s" % [name[0..-4], windows.size]
   puts windows.map{|w|w['id']}.join(", ")
   puts
