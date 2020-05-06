b6b6d33a51560bbcd25db33f3a67aa87a089f527
Richard Drake <rdrake98@gmail.com>
2020-05-06 06:19:43 +0100

    use Struct Backup

--- bin/show_windows.rb
+++ bin/show_windows.rb
@@ -4,8 +4,7 @@ require 'json'
 
 Backup = Struct.new(:name, :windows)
 Dir.chdir(ENV['tab_backups'])
-backups = Dir.glob("s*p.js").sort.map do |name|
-  Backup.new(name[0..-4],
-    JSON.parse(File.read(name)[2..-1])["sessions"][0]["windows"].map{|w|w['id']})
+backups = Dir.glob("s*p.js").sort.map do |name| Backup.new(name[0..-4],
+  JSON.parse(File.read(name)[2..-1])["sessions"][0]["windows"].map{|w|w['id']})
 end
 puts backups
