05c515ed9253d274fd16bea1c4e9c0c6a8f37efd
Richard Drake <rdrake98@gmail.com>
2020-05-07 08:53:11 +0100

    checking uniqueness of backup name

--- bin/show_backups.rb
+++ bin/show_backups.rb
@@ -3,11 +3,16 @@
 require 'json'
 require 'date'
 
+names = []
 Dir.chdir(ENV['tab_backups'])
 Dir.glob("s*.js").sort.each do |name|
+  names << name[1..6] + name[8..9] + name[14]
   windows = JSON.parse(File.read(name)[2..-1])["sessions"][0]["windows"]
   tabs = windows.map {|win| win["tabs"]}.flatten.size
   machine = "m#{name[14]}"
   time = DateTime.parse("#{name[1..6]} #{name[8..13]}").strftime("%d %b %H:%M")
   puts "%s %s: %2d windows, %3d tabs" % [time, machine, windows.size, tabs]
 end
+puts names.size
+puts names.uniq.size
+puts names[0]
