17ccc5e0c21fdbbd403168ef3116ae2ca2738498
Richard Drake <rdrake98@gmail.com>
2020-05-09 00:33:17 +0100

    show_backups checks for duplicates, has argument for just this

--- bin/show_backups.rb
+++ bin/show_backups.rb
@@ -7,10 +7,14 @@ names = []
 Dir.chdir(ENV['tab_backups'])
 Dir.glob("s*.js").sort.each do |name|
   names << name[1..6] + name[8..9] + name[14]
-  windows = JSON.parse(File.read(name)[2..-1])["sessions"][0]["windows"]
-  tabs = windows.map {|win| win["tabs"]}.flatten.size
-  machine = "m#{name[14]}"
-  time = DateTime.parse(name[1..13].gsub('.',' ')).strftime("%d %b %H:%M")
-  puts "%s %s: %2d windows, %3d tabs" % [time, machine, windows.size, tabs]
+  unless ARGV[0]
+    windows = JSON.parse(File.read(name)[2..-1])["sessions"][0]["windows"]
+    tabs = windows.map {|win| win["tabs"]}.flatten.size
+    machine = "m#{name[14]}"
+    time = DateTime.parse(name[1..13].gsub('.',' ')).strftime("%d %b %H:%M")
+    puts "%s %s: %2d windows, %3d tabs" % [time, machine, windows.size, tabs]
+  end
 end
-puts names.size, names.uniq.size, names[0]
+print names.size
+duplicates = names.size - names.uniq.size
+puts " " + (duplicates > 0 ? "** #{duplicates} duplicates **" : "")
