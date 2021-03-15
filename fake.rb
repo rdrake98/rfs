ba1f71c2252bb31b15e3ac1fa9b63c79d601e7c2
Richard Drake <rdrake98@gmail.com>
2021-03-15 21:53:10 +0000

    sorted glob

--- fake.rb
+++ fake.rb
@@ -1,9 +1,11 @@
 # fake.rb
 
+require 'base'
+
 if ARGV[0] == 'e'
   require './test/test_externals.rb'
 elsif ARGV[0]
   require './test/quick_fat.rb'
 else
-  Dir.glob('./test/test*.rb')[1..-2].each { |file| require file }
+  Dir.glob('./test/test*.rb')[2..-1].each { |file| require file }
 end
