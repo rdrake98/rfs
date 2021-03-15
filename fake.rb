0f4b986d976b5fa587737c7916d7ddb8e6f97b26
Richard Drake <rdrake98@gmail.com>
2021-03-15 19:04:30 +0000

    leave out test_link for now

--- fake.rb
+++ fake.rb
@@ -5,5 +5,5 @@ if ARGV[0] == 'e'
 elsif ARGV[0]
   require './test/quick_fat.rb'
 else
-  Dir.glob('./test/test*.rb')[2..-1].each { |file| require file }
+  Dir.glob('./test/test*.rb')[1..-2].each { |file| require file }
 end
