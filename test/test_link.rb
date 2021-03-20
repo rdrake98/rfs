51d4b949c0cc2e6f4e7c6a9b2b9b7c9023f9f9df
Richard Drake <rdrake98@gmail.com>
2021-03-20 00:28:51 +0000

    test_link fixed

--- test/test_link.rb
+++ test/test_link.rb
@@ -5,21 +5,21 @@ require 'splitter'
 
 module Minitest::Assertions
   def assert_has_line(content, line)
-    assert_includes(content.split("\n"), line)
+    assert_includes(content[0].split("\n"), line)
   end
 end
 
 class TestLink < MiniTest::Test
-  wiki_path = Dir.data "dev_.html"
+  wiki_path = Dir.data "dev_for_link.html"
   if ARGV[0] == "c"; `cp $dev #{wiki_path}`; end
   wiki = Splitter.new(wiki_path)
   wiki.titles.grep(/^TL\d+$/).each do |name|
     describe name do
       tests = wiki["#{name}A"].content.split("----\n")
       tests.each do |test|
-        search_text, result = test.split("\n")
-        it search_text do
-          assert_has_line(wiki[name].link(search_text, false, false), result)
+        search, result = test.split("\n")
+        it search do
+          assert_has_line(wiki[name].link(search, nil, false, false), result)
         end
       end
     end
