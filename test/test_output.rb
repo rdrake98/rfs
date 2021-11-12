13b3ccc11b26a48bf64b299958a74a8ffa369d32
Richard Drake <rdrake98@gmail.com>
2021-11-12 06:43:05 +0000

    fixing troublesome urls

--- test/test_output.rb
+++ test/test_output.rb
@@ -10,18 +10,31 @@ class TestOutput < MiniTest::Test
   path = Dir.data "#{type}_output.html"
   wiki = Splitter.new(wiki_path)
   limit = few && wiki["RubyTests"].tiddler_links.size
+  puts limit
   one = ARGV[1]
   all = !few && !one
+  re = /src=".*?"/
+  re2 = /href="txmt:.*?"/
+  last_name = "--"
+  count = 0
   describe "all" do
     Regex.scan_output(path).each_with_index do |chunk, i|
       name, output = chunk
       if all || few && (i < limit || name =~ /RFF\d\d/) || one && name == one
+        last_name = name
+        count += 1
         it "output for '#{name}'" do
-          assert_equal(output, wiki[name].output)
+          output.gsub!(re, 'src="image/URL"')
+          output.gsub!(re2, 'src="textmate/URL"')
+          wiki_output = wiki[name].output.gsub(re, 'src="image/URL"')
+          wiki_output.gsub!(re2, 'src="textmate/URL"')
+          assert_equal(output, wiki_output)
         end
       end
     end
   end
+  puts last_name if few
+  puts count if few
   puts
   puts "ruby -e 'require \"Splitter\"; Splitter.#{type}[\"#{one}\"].output;' dd" if one
   puts
