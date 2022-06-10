4829780c9cc886623a5908f0d2377fd6068343d6
Richard Drake <rdrake98@gmail.com>
2022-06-10 13:47:47 +0100

    read_filerd to begin to look at local copies

---
+++ bin/read_filerd.rb
@@ -0,0 +1,19 @@
+# read_filerd.rb
+
+require 'splitter'
+
+dir = ARGV[0] || "rd"
+$fat = Splitter.fat
+tiddlers = $fat.tiddlers
+puts tiddlers.size
+$focus = tiddlers.select {|t| t.content.index("file:///Users/#{dir}/")}
+puts $focus.size
+re = /file:\/\/\/Users\/#{dir}\/(.*?)(\]\]|\s)/
+xx = Struct.new(:title, :sequel)
+$sequels = $focus.map{|t|
+  t.content.scan(re).map do
+    xx.new(t.title, $1)
+  end
+}.flatten
+puts $sequels.size
+$sequels.each {|x| puts "#{x.title} --- #{x.sequel}"}
