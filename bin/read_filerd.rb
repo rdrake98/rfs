9b0330d602533304e11b9599507437f81095b115
Richard Drake <rdrake98@gmail.com>
2022-06-10 14:09:53 +0100

    tidy

--- bin/read_filerd.rb
+++ bin/read_filerd.rb
@@ -10,10 +10,10 @@ $focus = tiddlers.select {|t| t.content.index("file:///Users/#{dir}/")}
 puts $focus.size
 re = /file:\/\/\/Users\/#{dir}\/(.*?)(\]\]|\s)/
 xx = Struct.new(:title, :sequel)
-$sequels = $focus.map{|t|
+$sequels = $focus.map do |t|
   t.content.scan(re).map do
     xx.new(t.title, $1)
   end
-}.flatten
+end.flatten
 puts $sequels.size
 $sequels.each {|x| puts "#{x.title} --- #{x.sequel}"}
