84973adb7222b924988c29ad9795f3fb3a39d63c
Richard Drake <rdrake98@gmail.com>
2022-06-12 16:21:34 +0100

    using Benchmark

--- models/wiki.rb
+++ models/wiki.rb
@@ -1,13 +1,17 @@
 # wiki.rb
 
 require 'splitter'
+require 'benchmark'
 
 class Wiki < Splitter
   def test_urls
     $t1 = true
     $a = []
     $aa = Struct.new(:tiddler, :url)
-    tiddlers.each(&:output)
+    puts Benchmark.measure {tiddlers.each(&:output)}
+    puts Benchmark.measure {tiddlers.each(&:output)}
+    puts Benchmark.measure {tiddlers.each(&:external_links)}
+    puts Benchmark.measure {tiddlers.each(&:external_links)}
     puts $a.size
   end
 end
