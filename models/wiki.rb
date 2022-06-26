efa24c7990fd46fc56f3b7ea128607d2fa71797e
Richard Drake <rdrake98@gmail.com>
2022-06-26 14:27:40 +0100

    remove premature optimisation of references

--- models/wiki.rb
+++ models/wiki.rb
@@ -34,28 +34,6 @@ class Wiki < Splitter
     f1
   end
 
-  def warm_references
-    puts tiddlers.size
-    full = normal_tiddlers.map do |t|
-      t.tiddler_links.map { |link| Ref.new(t.title, link) }
-    end.flatten
-    puts full.size
-    links = full.group_by &:link
-    puts links.size
-    linking_tiddlers = links.keys.group_by {|s| self.referent(s)}
-    puts linking_tiddlers.size
-    linking_tiddlers.each do |tiddler, refs|
-      titles = refs.map{|r|links[r]}.flatten.map(&:linker).uniq # uniq needed?
-      if tiddler
-        tiddler.references =
-          titles.sort_by{ |title| self.splitName(title).downcase }.
-          map { |title| self[title] }
-      else
-        puts titles.size
-      end
-    end
-  end
-
   def show_scripts
     puts @mid.lines[-1]
     puts @after.lines[0..4]
