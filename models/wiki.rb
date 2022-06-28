b192542dde682d7ec036ce4da36f885cd0310bf1
Richard Drake <rdrake98@gmail.com>
2022-06-28 13:52:00 +0100

    attempted name_patch_analysis

--- models/wiki.rb
+++ models/wiki.rb
@@ -21,4 +21,54 @@ class Wiki < Splitter
     write
     # `open #{new_name}`
   end
+
+  def Wiki.name_patch_analysis
+    f1 = fat
+    np = f1["NamePatches"]
+    lines = np.content.lines.map(&:chomp).reject{|s|s==""}
+    puts lines.size
+    wiki_links = lines.select{|s|WikiText.isWikiLink(s)}
+    tids = f1.tiddlers - [np]
+    puts tids.size
+    tids.reject! do |t|
+      t.tiddler_links.any? { |s| !f1.referent(s) }
+    end
+    puts tids.size
+
+    dwiki_links = wiki_links.select { |s| !f1.referent(s) }
+    puts "", dwiki_links.size
+    dmatches = dwiki_links.map do |wl|
+      ts = tids.select do |t|
+        t.tiddler_links.any? do |s|
+          s == wl
+        end
+      end
+      if ts.size > 0
+        puts wl
+        puts ts.size
+        ts.map{|t| [wl,t.title]}
+      else
+        nil
+      end
+    end.compact
+    puts dmatches.size
+
+    puts "", wiki_links.size
+    matches = wiki_links.map do |wl|
+      ts = tids.select do |t|
+        t.tiddler_links.any? do |s|
+          !f1.referent(s) && s=~/(#{wl}\w|\w#{wl})/
+        end
+      end
+      if ts.size > 0
+        puts wl
+        puts ts.size
+        ts.map{|t| [wl,t.title]}
+      else
+        nil
+      end
+    end.compact
+    puts matches.size
+    [f1, wiki_links, matches, dmatches]
+  end
 end
